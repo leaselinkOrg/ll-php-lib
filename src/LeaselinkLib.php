ee3b10a996c98db5656afa14368145209d598425
Adam Grzywacz <adam.grzywacz@leaselink.pl>
2025-02-19 15:47:39 +0100

    Bump version to 1.0.1 in composer.json and LeaseLinkLib class

--- src/LeaselinkLib.php
+++ src/LeaselinkLib.php
@@ -23,7 +23,7 @@ use LeaseLink\Model\NotificationData;
  */
 final class LeaseLinkLib
 {
-    private const VERSION = '1.0.0';
+    private const VERSION = '1.0.1';
 
     /**
      * Create a new LeaseLink library instance
